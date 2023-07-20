6a71aa8d510ee949a2735ade1685f78dfb080221
こうへい <rugby.kohei.warabies@gmail.com>
2023-07-20 22:23:42 +0900

    色々やりました！

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_09_070137) do
+ActiveRecord::Schema.define(version: 2023_07_18_215225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "boards", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -26,10 +47,14 @@ ActiveRecord::Schema.define(version: 2023_07_09_070137) do
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.bigint "board_id", null: false
     t.string "title", null: false
     t.text "content", null: false
+    t.date "date", null: false
+    t.string "eyecatch"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_tasks_on_board_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
@@ -45,4 +70,5 @@ ActiveRecord::Schema.define(version: 2023_07_09_070137) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
