3a92d2a79409e6aba85dd7d9b14658dafd16b7cb
こうへい <rugby.kohei.warabies@gmail.com>
2023-07-26 23:05:16 +0900

    profileを作成しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  delegate :birthday, :gender , :age , to: :profile ,allow_nil: true
 
   def has_written?(board)
     boards.exists?(id: board.id)
@@ -17,5 +20,22 @@ class User < ApplicationRecord
   end
 
   def display_icon
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'Oval.svg'
+    end
+  end
+
+  def birthday
+    profile&.birthday
+  end
+
+  def gender
+    profile&.gender
+  end
+  
+  def display_name
+    profile&.nickname || self.email.split('@').first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
       resources :comments
     end
   end
+
+  resource :profile, only: [:show, :edit, :update]
 end
